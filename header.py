547c6e5219970e914d40c27639ab1da4b9913d64
william templier <templier.w56@gmail.com>
2021-11-19 22:56:23 +0100

    pysparkdf

--- header.py
+++ header.py
@@ -4,7 +4,10 @@ table='machine_events'
 
 def get_table_header(table):
     with open('schema.csv') as schema:
-        header=  [ line.split(',')[2] for line in schema.readlines() if re.match(f'^{table}/*', line) ]
+        header=  [
+            line.split(',')[2].lower().replace(' ', '_')\
+            for line in schema.readlines() if re.match(f'^{table}/*', line) 
+            ]
 
     return header
 
