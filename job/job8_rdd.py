35662af13afc98a2991f34d6e7da00b56fc8243c
Vincent AUBRIOT <vincent3897@gmail.com>
2022-01-10 18:36:15 +0100

    edited job8-rdd

--- job/job8_rdd.py
+++ job/job8_rdd.py
@@ -18,36 +18,30 @@ def job_8():
     start = time.time()
 
     # Select first SUBMIT transition for each job
-    submit_status = te.select(['job_id','task_index','event_type', 'time'])\
-        .sample(False, sample)\
+    submit_status = te.select(['job_id','event_type', 'time'])\
         .filter(lambda x: x[1] in ['0'])\
-    .map(lambda x: (x[0], float(x[2])))
+        .map(lambda x: (x[0], float(x[2])))
 
     rdd_submit = submit_status.groupByKey().mapValues(lambda x: round(sum(x)/len(x), 3))
     
-    # Select first SUBMIT transition for each job
-    outpending_status = te.select(['job_id','task_index','event_type', 'time'])\
-        .sample(False, sample)\
+    # Select first OUT transition for each job
+    outpending_status = te.select(['job_id','event_type', 'time'])\
         .filter(lambda x: x[1] in ['1', '3', '5', '6'])\
-    .map(lambda x: (x[0], float(x[2])))
+        .map(lambda x: (x[0], float(x[2])))
 
     rdd_out = outpending_status.groupByKey().mapValues(lambda x: round(sum(x)/len(x), 3))
 
     rdd_deltatimes = rdd_submit.join(rdd_out).map(lambda x: (x[0], x[1][1] - x[1][0]))
 
-    # Load the task_constraints table
-
     # Selects each occurence of constraint registered for each process
-    task_constraints_per_jobtask = tc.select(['job_id', 'task_index', 'time']).map(lambda x: ((x[0],x[1]), x[2]))
+    task_constraints_per_job = tc.select(['job_id'])\
+        .map(lambda x: (x[0],1))\
+        .reduceByKey(lambda a,b: a+b)
 
-    # Counts the total number of constraints for each process
-    rdd_number_task_constraints_per_jobtask = task_constraints_per_jobtask.combineByKey(count_init, count_merge, count_cmb).map(lambda x: ((x[0][0]), x[1][0]))
-
-        # Join the delta time and the number of constraints in one RDD
-    rdd_delta_constraints = rdd_deltatimes.join(rdd_number_task_constraints_per_job).filter(lambda x: x[1][1] < 50)
+    rdd_delta_constraints = rdd_deltatimes.join(task_constraints_per_job).filter(lambda x: x[1][1] < 50)
 
     # Create the list of values from the last RDD
-    rdd_to_map = rdd_delta_constraints.sample(False, sample * 2).collectAsMap()
+    rdd_to_map = rdd_delta_constraints.collectAsMap()
     deltatimes = [v[0] for v in rdd_to_map.values()]
     constraints = [v[1] for v in rdd_to_map.values()]
 
